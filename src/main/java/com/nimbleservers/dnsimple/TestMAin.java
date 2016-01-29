d28c757c4b96a952c07549f4e637b56e6f9cab6f
DillonFancher <dfancher@twitter.com>
2016-01-29 13:21:58 -0700

    df: work in progress on getting this library working.

---
+++ src/main/java/com/nimbleservers/dnsimple/TestMAin.java
@@ -0,0 +1,17 @@
+package com.nimbleservers.dnsimple;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.nimbleservers.dnsimple.record.Record;
+
+public class TestMAin {
+
+    private static DnsimpleContext dns = new DnsimpleContext("w9gZn3Xso2AJ32yibEHKeZc6tpvzXcUT", "https://api.dnsimple.com/v1");
+
+    public static void main(String[] args) throws IOException, UnexpectedResponseException {
+        List<Record> a = dns.getRecords("webyoso.com");
+        System.out.println(a);
+    }
+
+}
